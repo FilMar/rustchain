24ea47b258a14476b53bd86617d3ecea3e58569a
filmar <filippo.ufficiale@gmail.com>
2024-06-28 18:43:03 +0200

    now hash of block is dynamic and verify drop broken chain

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use sha2::{Digest, Sha256};
 
 trait BlockChain {
     fn create_block(&mut self, data: String);
-    fn validate_block(&self, block: &mut Block) -> (String, Block);
-    fn get_last_block(&self) -> (String, Block);
-    fn verify_chain(&self) -> bool;
+    fn validate_block(&self, block: &mut Block) -> Block;
+    fn get_last_block(&self) -> Block;
+    fn verify_chain(&mut self) -> bool;
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -21,7 +21,7 @@ struct Block {
 
 struct Chain {
     proof: u8,
-    chain: Vec<(String, Block)>,
+    chain: Vec<Block>,
 }
 
 impl Block {
@@ -47,43 +47,45 @@ impl Block {
 
 impl BlockChain for Chain {
     fn create_block(&mut self, data: String) {
-        let (pre_hash, last_block) = self.get_last_block();
-        let mut new_block = Block::new(last_block.index + 1, data, pre_hash);
+        let last_block = self.get_last_block();
+        let mut new_block = Block::new(last_block.index + 1, data, last_block.hash());
         let validated_block = self.validate_block(&mut new_block);
         self.chain.push(validated_block)
     }
 
-    fn validate_block(&self, block: &mut Block) -> (String, Block) {
+    fn validate_block(&self, block: &mut Block) -> Block {
         let mut nonce = 1u64;
         loop {
             block.nonce = Some(nonce);
             let hash = block.hash();
             if self.proof_of_work(&hash) {
-                return (hash, block.clone());
+                return block.clone();
             }
             nonce += 1;
         }
     }
 
-    fn get_last_block(&self) -> (String, Block) {
+    fn get_last_block(&self) -> Block {
         match self.chain.last() {
-            Some((hash, block)) => (hash.clone(), block.clone()),
+            Some(block) => block.clone(),
             None => panic!("No block in the chain"),
         }
     }
 
-    fn verify_chain(&self) -> bool {
+    fn verify_chain(&mut self) -> bool {
         let mut prev_hash: Option<String> = None;
-        for (hash, block) in self.chain.iter() {
-            if !self.proof_of_work(&hash) {
+        for block in self.chain.iter() {
+            if !self.proof_of_work(&block.hash()) {
+                self.drop_dead_blocks(block.index);
                 return false;
             }
             if let Some(prev) = prev_hash {
                 if prev != block.prev_hash {
+                    self.drop_dead_blocks(block.index);
                     return false;
                 }
             }
-            prev_hash = Some(hash.clone());
+            prev_hash = Some(block.hash());
         }
         true
     }
@@ -107,6 +109,16 @@ impl Chain {
         blockchain
     }
 
+    fn drop_dead_blocks(&mut self, index: u64) {
+        while let Some(block) = self.chain.last() {
+            if block.index >= index {
+                self.chain.pop();
+            } else {
+                break;
+            }
+        }
+    }
+
     fn proof_of_work(&self, hash: &str) -> bool {
         hash.starts_with(&"0".repeat(self.proof as usize))
     }
@@ -119,6 +131,8 @@ fn main() {
     blockchain.create_block("Blockchain".to_string());
     blockchain.create_block("Rust".to_string());
     blockchain.create_block("Ciao".to_string());
+    println!("{:?}", blockchain.verify_chain());
+    blockchain.chain[4].data = "Ciao Mondo".to_string();
     println!("{:?}", blockchain.verify_chain());
     println!(
         "{}",
